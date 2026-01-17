7d9043c859ea0007576744391f7c2976b6f1ef7b
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2026-01-17 13:16:42 +0300

    +

--- Topics/Search in a string/Theory/main.py
+++ Topics/Search in a string/Theory/main.py
@@ -1,2 +1,11 @@
-#  You can experiment here, it won’t be checked
+five = "one two three four five "
+print(five.rfind("f"))
+print(five.count(" "))
+# print(five.rindex("w", 3, 5))
+print(five.index("w"))
+print(five.find("t"))
 
+print("-----")
+tongue_twister = 'Through three cheese trees, three free fleas flew'
+print(tongue_twister.rfind('ee'))
+print(tongue_twister.count('ee'))
