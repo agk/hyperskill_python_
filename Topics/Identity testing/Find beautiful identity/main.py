6aba31adb154faed8a55eb8763675f75057c1c19
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2026-01-17 11:24:05 +0300

    +

--- Topics/Identity testing/Find beautiful identity/main.py
+++ Topics/Identity testing/Find beautiful identity/main.py
@@ -1,5 +1,6 @@
 def object_with_beautiful_identity():
     for i in range(10_000):
-        # Change the next line
-        if ...:
+        if id(i) % 1000 == 888:
             return i
+
+# object_with_beautiful_identity()
--- Topics/Identity testing/Theory/main.py
+++ Topics/Identity testing/Theory/main.py
@@ -1,2 +1,8 @@
 #  You can experiment here, it won’t be checked
+a = 100000
+b = 100000
 
+print(bool(a is b))
+print(bool(id(a) == id(b)))
+print(bool(a == b))
+print(bool(id(a) is id(b)))
