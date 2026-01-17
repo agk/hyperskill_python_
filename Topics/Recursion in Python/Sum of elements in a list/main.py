efb6cf465c06e9d70fe8ff8d7b1933963a6ff64a
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2026-01-17 20:19:41 +0300

    recursion

--- Topics/Recursion in Python/Sum of elements in a list/main.py
+++ Topics/Recursion in Python/Sum of elements in a list/main.py
@@ -1,9 +1,13 @@
 def list_sum(some_list):
+    #print(some_list)
     if some_list == []:
         return 0
     # base case
-    elif ...:  
+    elif len(some_list) == 1:
         return some_list[0]
     # recursive case
     else:
-        return
+        return sum(some_list)
+
+# int_list = list(map(int, input().split(" ")))
+# print(list_sum(int_list))
