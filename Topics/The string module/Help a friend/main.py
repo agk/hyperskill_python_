c55f083959e4db0f91eceaa5fce6a50903ec1073
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2026-01-17 15:19:37 +0300

    strings

--- Topics/The string module/Help a friend/main.py
+++ Topics/The string module/Help a friend/main.py
@@ -1,5 +1,6 @@
-template = string.Template("Hi, $name! You look adjective today! You're doing great!")
+import string
+template = string.Template("Hi, $name! You look $adjective today! You're doing great!")
 name = input()
 adjective = input()
-compliment = template.format(name=name, adjective)
+compliment = template.substitute(name=name, adjective=adjective)
 print(compliment)
