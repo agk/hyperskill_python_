e03f63b53d4ca7246146e27efcfb846b73011a0f
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2026-02-01 18:36:39 +0300

    +

--- Topics/Default arguments/Fan or not/main.py
+++ Topics/Default arguments/Fan or not/main.py
@@ -1,2 +1,13 @@
-def add_viewer(...):
-    ...
+def add_viewer(name, fan_list=None):
+    if fan_list is not None:
+
+        fan_list.append(name)
+    else:
+        fan_list = []
+        fan_list.append(name)
+
+    return fan_list
+
+
+# print(add_viewer("all"))
+# print(add_viewer("all", ["sss", "dddd"]))
