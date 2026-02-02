f2478c55076c2bef0aeb3a4fc30579f6725aafeb
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2026-02-02 09:47:33 +0300

    +

--- Topics/Args/Numbers/main.py
+++ Topics/Args/Numbers/main.py
@@ -3,10 +3,12 @@ def multiply(a, *nums):
     if len(nums) == 0:
         return a * 1
 
-    total = a * a
+    total = a
     for n in nums:
         total *= n
 
     return total
 
-print(multiply(*[1, 2, 3]))
+# print(multiply(*[1, 2, 3]))
+# print(multiply(2))
+# print(multiply(*[1, 2, 3]))
