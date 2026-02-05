9f80de5a8cdeedaa3cdb4fd96028f965ab414345
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2026-02-05 16:14:49 +0300

    +

--- Topics/Args/Congratulatory letter/main.py
+++ Topics/Args/Congratulatory letter/main.py
@@ -1,2 +1,9 @@
-def congratulations():
-    pass
+def congratulations(manager_name, testing_name, *developer):
+    message = "Happy New Year! Take care of yourself and your loved ones!\nFor:\n"
+    message += manager_name + "\n"
+    message += testing_name + "\n"
+
+    for name in developer:
+        message += name + "\n"
+
+    print(message)
